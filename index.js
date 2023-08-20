02817f7c1d4f9e342291a2220ff3272bed0ef924
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-08-20 12:28:35 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(data, target){
+    const cb = (name) => name.toUpperCase() === target.toUpperCase();
+    return data.filter(cb);
+}
+
+function fuzzyMatch(data, target){
+    const cb = (name) => name.slice(0,target.length).toUpperCase() === target.toUpperCase();
+    return data.filter(cb)
+}
+
+function matchName(data, target){
+    const cb = (element) => element.name.toUpperCase() === target.toUpperCase();
+    return data.filter(cb);
+}
